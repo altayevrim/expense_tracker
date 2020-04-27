4d9a5b65d8495b1beb87c7eb189c194f82befa37
altayevrim <evrimaltay@gmail.com>
2020-04-27 04:37:55 +0300

    bir sürü şey eklendi, önce nasıl uygulamayı sadece bir rotasyonda kullanmayı gösterirken, ek olarak da farklı (yatay/dikey ) modlarda farklı tepki vermesini sağladık

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import './widgets/new_transaction.dart';
+import 'package:flutter/services.dart';
 
 import './widgets/chart.dart';
 import './widgets/transaction_list.dart';
@@ -6,7 +7,14 @@ import './models/transaction.dart';
 import './models/currency.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  //   DeviceOrientation.portraitDown,
+  // ]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -68,6 +76,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // ),
   ];
 
+  bool _showChart = false;
+
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((tx) {
       return tx.date.isAfter(
@@ -111,28 +121,64 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Expense Tracker by Rimtay',
-          // style: TextStyle(
-          //   fontFamily: 'Open Sans',
-          // ),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () => _startAddNewTransaction(context),
-          ),
-        ],
+    final bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+    final appBar = AppBar(
+      title: Text(
+        'Expense Tracker by Rimtay',
+        // style: TextStyle(
+        //   fontFamily: 'Open Sans',
+        // ),
       ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () => _startAddNewTransaction(context),
+        ),
+      ],
+    );
+    final txListWidget = Container(
+      height: (MediaQuery.of(context).size.height * 0.7) -
+          appBar.preferredSize.height -
+          MediaQuery.of(context).padding.top,
+      child: TransactionList(_userTransactions, _currency, _deleteTransaction),
+    );
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(_recentTransactions, _currency),
-            TransactionList(_userTransactions, _currency, _deleteTransaction),
+            if (isLandscape)
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Show Chart'),
+                    Switch(
+                      value: _showChart,
+                      onChanged: (val) {
+                        setState(() {
+                          _showChart = val;
+                        });
+                      },
+                    ),
+                  ]),
+            if (!isLandscape) Container(
+                    height: (MediaQuery.of(context).size.height * 0.3) -
+                        appBar.preferredSize.height -
+                        MediaQuery.of(context).padding.top,
+                    child: Chart(_recentTransactions, _currency),
+                  ),
+            if (!isLandscape) txListWidget,
+            if (isLandscape) _showChart
+                ? Container(
+                    height: (MediaQuery.of(context).size.height * 0.7) -
+                        appBar.preferredSize.height -
+                        MediaQuery.of(context).padding.top,
+                    child: Chart(_recentTransactions, _currency),
+                  )
+                : txListWidget,
           ],
         ),
       ),
