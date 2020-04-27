8470cac35bb89afd6e4af10fd8f47e96409ebefb
altayevrim <evrimaltay@gmail.com>
2020-04-27 04:40:05 +0300

    resim ve benzeri şeyler kendi içinde bulunduğu alana göre özel olarak boyutlanıyor. LayoutBuilder ile yapıyor bunu

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,66 +11,70 @@ class TransactionList extends StatelessWidget {
   TransactionList(this._transactions, this._currency, this._deleteTransaction);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      child: _transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text('No transactions added yet!',
-                    style: Theme.of(context).textTheme.title),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                  height: 200,
-                ),
-              ],
-            )
-          : ListView.builder(
-              itemCount: _transactions.length,
-              itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    leading: Container(
-                      height: 60,
-                      width: 60,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        shape: BoxShape.circle,
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text(
-                              '${_currency.left()}${_transactions[index].amount}${_currency.right()}',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold)),
-                        ),
-                      ),
-                    ),
-                    title: Text(_transactions[index].title,
+    return _transactions.isEmpty
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: <Widget>[
+                  Container(
+                    height: constraints.maxHeight * 0.15,
+                    child: Text('No transactions added yet!',
                         style: Theme.of(context).textTheme.title),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(_transactions[index].date),
+                  ),
+                  SizedBox(
+                    height: constraints.maxHeight * 0.15,
+                  ),
+                  Container(
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
                     ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        _deleteTransaction(_transactions[index].id);
-                      },
-                      color: Theme.of(context).errorColor,
+                    height: constraints.maxHeight * 0.7,
+                  ),
+                ],
+              );
+            },
+          )
+        : ListView.builder(
+            itemCount: _transactions.length,
+            itemBuilder: (ctx, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: ListTile(
+                  leading: Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text(
+                            '${_currency.left()}${_transactions[index].amount}${_currency.right()}',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold)),
+                      ),
                     ),
                   ),
-                );
-              },
-            ),
-    );
+                  title: Text(_transactions[index].title,
+                      style: Theme.of(context).textTheme.title),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(_transactions[index].date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      _deleteTransaction(_transactions[index].id);
+                    },
+                    color: Theme.of(context).errorColor,
+                  ),
+                ),
+              );
+            },
+          );
   }
 }
